ac100a0aeee208f7ee6146824293b9417f6c1d49
cruelwxrld <146163829+cruelwxrld@users.noreply.github.com>
2025-04-23 13:15:46 +0000

    Pass 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,2 +1,8 @@
+from src.say_hello import say_hello
+
 def main():
-    print("Hello World")
+    print("Welcome to the Brain Games!")
+    print(f'Hello, {say_hello()}')
+
+if __name__ == '__main__':
+    main()
---
+++ src/say_hello.py
@@ -0,0 +1,2 @@
+def say_hello():
+    return input('May I have your name? ')
