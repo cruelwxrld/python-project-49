8758a7c23f04be070ab4b10143c0b11438334903
Dmitry <cruelwxrlddd@gmail.com>
2025-05-22 10:56:53 +0000

    The seventh stage of development is completed

--- brain_games/__init__.py
+++ brain_games/__init__.py
@@ -1,6 +1,6 @@
 from brain_games.cli import say_answer, welcome_user
-from brain_games.games.brain_calc import properties as properties_calc
-from brain_games.games.brain_even import properties as properties_even
-from brain_games.games.brain_gcd import properties as properties_gcd
+from brain_games.scripts.brain_calc import properties as properties_calc
 from brain_games.scripts.brain_engine import engine, run_game
+from brain_games.scripts.brain_even import properties as properties_even
 from brain_games.scripts.brain_games import main as brain_main
+from brain_games.scripts.brain_gcd import properties as properties_gcd
