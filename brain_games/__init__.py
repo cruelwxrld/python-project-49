830b06f82a07b2dd29768bb76d8b1701dfe3c877
cruelwxrld <146163829+cruelwxrld@users.noreply.github.com>
2025-10-01 00:18:47 +0800

    Update __init__.py

--- brain_games/__init__.py
+++ brain_games/__init__.py
@@ -1,5 +1,5 @@
+from brain_games.cli import get_name, say_answer, welcome_user
 from brain_games.games.brain_engine import engine, run_game
-from brain_games.cli import say_answer, welcome_user, get_name
 from brain_games.scripts.brain_calc import properties as properties_calc
 from brain_games.scripts.brain_even import properties as properties_even
 from brain_games.scripts.brain_games import main as brain_main
